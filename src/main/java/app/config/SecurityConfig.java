0029a0f7dc1cc3dba2a34065c8d7ad782060078d
Alex Balak <alex.balak@outlook.com>
2025-06-17 19:10:51 +0200

    expanded the lambda method for csrf customizer -> disabled

--- src/main/java/app/config/SecurityConfig.java
+++ src/main/java/app/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 //Configuring Spring Security
@@ -18,7 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         //Diable csrf
-        http.csrf(AbstractHttpConfigurer::disable);
+        //http.csrf(customizer -> customizer.disable());
 
 
         //Authentication needed for any request
@@ -26,6 +28,26 @@ public class SecurityConfig {
 
         //Enabling login with default page
         http.formLogin(Customizer.withDefaults());
+
+        //Enabling basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // Disable session
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+
+        // Example of Lambda method expanded from the above code
+        Customizer<CsrfConfigurer<HttpSecurity>> customCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CsrfConfigurer<HttpSecurity> customizer) {
+                customizer.disable();
+            }
+        };
+        http.csrf(customCsrf);
+
+
+
 
 
         return http.build();
