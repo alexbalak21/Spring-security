bd4201b0fd5c4906f4e1cff1fa22bac380383ef3
Alex Balak <alex.balak@outlook.com>
2025-06-17 18:35:13 +0200

    Update to SecurityConfig

--- src/main/java/app/config/SecurityConfig.java
+++ src/main/java/app/config/SecurityConfig.java
@@ -2,16 +2,31 @@ package app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+//Configuring Spring Security
 @Configuration
+//Enabling Spring Security
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        //Diable csrf
+        http.csrf(AbstractHttpConfigurer::disable);
+
+
+        //Authentication needed for any request
+        http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
+
+        //Enabling login with default page
+        http.formLogin(Customizer.withDefaults());
+
 
         return http.build();
 
