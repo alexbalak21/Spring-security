a8effac2e19a276c59cf6e2fc1354d1d529bd6f5
Alex Balak <alex.balak@outlook.com>
2025-06-21 14:10:22 +0200

    working login for good

---
+++ src/main/java/app/spring/controller/AuthController.java
@@ -0,0 +1,35 @@
+package app.spring.controller;
+
+import app.spring.dto.LoginRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/api/login")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                ));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        //RETURN USER DETAILS
+        return ResponseEntity.ok(new HashMap<>());
+    }
+}
